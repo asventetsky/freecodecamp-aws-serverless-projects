f090fd6f3bdc06f3eab104fe8c804e6247fb54d8
Artyom Sventetsky <artyom.sven@gmail.com>
2022-09-19 23:51:34 +0600

    Implement jokes fetching

--- combination_api_project/source/api_composer.py
+++ combination_api_project/source/api_composer.py
@@ -1,11 +1,32 @@
-from webbrowser import get
 import requests
+
+URL = 'https://icanhazdadjoke.com/'
+headers = {'Accept': 'application/json'}
 
 def lambda_handler(event, context):
 
-    response = requests.get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/kzt.json")
-    
-    return {
+    result = {
         'statusCode' : 200,
-        'body': response.text
+        'headers': {
+            'Content-Type': 'application/json'
+        }
     }
+
+    try:
+        joke1_response = requests.get(URL, headers=headers)
+        joke2_response = requests.get(URL, headers=headers)
+        joke1 = joke1_response.json()['joke']
+        joke2 = joke2_response.json()['joke']
+    except Exception as error:
+        print('Internal server error', error)
+        result['statusCode'] = 500
+        result['body'] = {
+            'error': 'Internal server error'
+        }
+    else:
+        result['body'] = {
+            'joke_1': joke1,
+            'joke_2': joke2
+        }
+
+    return result
