0ef9cef431bdde841da73e325c6bf495bcad58dc
Artyom Sventetsky <artyom.sven@gmail.com>
2022-10-03 13:50:43 +0600

    parse trigger_datetime from the dynamodb's record

--- reminder-app/source/backend/lambda-reminder-event-create/lambda_reminder_event_create.py
+++ reminder-app/source/backend/lambda-reminder-event-create/lambda_reminder_event_create.py
@@ -1,7 +1,8 @@
 import boto3
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 import hashlib
+import json
 
 from botocore.exceptions import ClientError
 
@@ -10,7 +11,6 @@ lambda_client = boto3.client('lambda')
 
 
 def handler(event, context):
-
     print(f"Event from DynamoDB stream: {event}")
 
     for record in event['Records']:
@@ -21,44 +21,86 @@ def handler(event, context):
 
 
 def process_record(record):
-    notif_type = record['notification_type']['S']
-    trigger_datetime = record['trigger_datetime']['S']
-    message = record['message']['S']
-    print(f"Notif: {notif_type}, trigger datetime: {trigger_datetime}, message: {message}")
+    details = get_details(record)
 
-    d = datetime.now() + timedelta(minutes=1)
-    event_schedule = f"cron({d.minute} {d.hour} {d.day} {d.month} ? {d.year})"
-    print(f"Event schedule: {event_schedule}")
+    # UTC trigger datetime
+    trigger_datetime = details['trigger_datetime']
+    cron_trigger_datetime = f"cron({trigger_datetime.minute} {trigger_datetime.hour} {trigger_datetime.day} {trigger_datetime.month} ? {trigger_datetime.year})"
 
     lambda_function_name = os.environ["lambda_function_name"]
     lambda_function_arn = os.environ["lambda_function_arn"]
 
-    hash = hashlib.md5(event_schedule.encode()).hexdigest()
+    datetime_hash = hashlib.md5(cron_trigger_datetime.encode()).hexdigest()
+
+    event_rule_details = create_schedule_event_bridge_rule(cron_trigger_datetime, datetime_hash)
+    add_permission_to_invoke_lambda(lambda_function_name, event_rule_details['event_rule_arn'], datetime_hash)
+    add_target_for_event_bridge_rule(lambda_function_name, lambda_function_arn, event_rule_details['event_rule_name'],
+                                     details)
+
+
+def get_details(record):
+    notification_type = record['notification_type']['S']
+
+    # 2011-11-04T00:05:23
+    trigger_datetime_str = record['trigger_datetime']['S']
+    trigger_datetime = datetime.fromisoformat(trigger_datetime_str)
+
+    destination = record['user_id']['S']
+    message = record['message']['S']
+
+    details = {
+        "notification_type": notification_type,
+        "trigger_datetime": trigger_datetime,
+        "destination": destination,
+        "message": message
+    }
+    print(f"Details: {details}")
+
+    return details
+
+
+def create_schedule_event_bridge_rule(cron_trigger_datetime, datetime_hash):
+    event_rule_name = f"reminders-event-rule-{datetime_hash}"
 
     try:
-        event_rule_name = f"reminders-event-rule-{hash}"
         response = eventbridge_client.put_rule(
             Name=event_rule_name,
-            ScheduleExpression=event_schedule
+            ScheduleExpression=cron_trigger_datetime
         )
         event_rule_arn = response['RuleArn']
         print(f"Put rule {event_rule_name} with ARN {event_rule_arn}.")
+        return {
+            "event_rule_name": event_rule_name,
+            "event_rule_arn": event_rule_arn
+        }
     except ClientError:
         print(f"Couldn't put rule {event_rule_name}.")
         raise
 
+
+def add_permission_to_invoke_lambda(lambda_function_name, event_rule_arn, datetime_hash):
     try:
         lambda_client.add_permission(
-        FunctionName=lambda_function_name,
-        StatementId=f'{lambda_function_name}-{hash}-invoke',
-        Action='lambda:InvokeFunction',
-        Principal='events.amazonaws.com',
-        SourceArn=event_rule_arn
+            FunctionName=lambda_function_name,
+            StatementId=f'{lambda_function_name}-{datetime_hash}-invoke',
+            Action='lambda:InvokeFunction',
+            Principal='events.amazonaws.com',
+            SourceArn=event_rule_arn
         )
         print(f"Granted permission to let Amazon EventBridge call function {lambda_function_name}.")
     except ClientError:
         print(f"Couldn't add permission to let Amazon EventBridge call function {lambda_function_name}.")
         raise
+
+
+def add_target_for_event_bridge_rule(lambda_function_name, lambda_function_arn, event_rule_name, details):
+    destination = details['destination']
+    message = details['message']
+
+    input = {
+        "destination": destination,
+        "message": message
+    }
 
     try:
         response = eventbridge_client.put_targets(
@@ -67,7 +109,7 @@ def process_record(record):
                 {
                     'Id': lambda_function_name,
                     'Arn': lambda_function_arn,
-                    'Input': '{"name": "value"}'
+                    'Input': json.dumps(input)
                 }
             ]
         )
