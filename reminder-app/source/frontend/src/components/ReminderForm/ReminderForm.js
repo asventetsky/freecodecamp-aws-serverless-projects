963941934f0faaf50326457cf6608f5b8a9cdb69
Artyom Sventetsky <artyom.sven@gmail.com>
2022-10-18 22:10:40 +0600

    style form

--- reminder-app/source/frontend/src/components/ReminderForm/ReminderForm.js
+++ reminder-app/source/frontend/src/components/ReminderForm/ReminderForm.js
@@ -1,23 +1,27 @@
 import React from "react";
+import classes from "./ReminderForm.module.css"
 
 const ReminderForm = props => (
-    <form>
-        <h2 className="label-wrapper">
-            <label htmlFor="new-todo-input" className="label__lg">
-                What needs to be done?
-            </label>
-        </h2>
-        <input
-            type="text"
-            id="new-todo-input"
-            className="input input__lg"
-            name="text"
-            autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-            Add
-        </button>
-    </form>
+    <div className={classes.form}>
+        <form>
+            <h2 className="label-wrapper">
+                What needs to be notified?
+            </h2>
+            <ul className={classes.wrapper}>
+                <li className={classes.formRow}>
+                    <label htmlFor="message">Message</label>
+                    <input type="text" id="message"/>
+                </li>
+                <li className={classes.formRow}>
+                    <label htmlFor="datetime">Datetime</label>
+                    <input type="text" id="datetime"/>
+                </li>
+                <li className={classes.formRow}>
+                    <button type="submit">Add</button>
+                </li>
+            </ul>
+        </form>
+    </div>
 )
 
 export default ReminderForm;
