cf2262da520e1457f5b562f8a91ce2da9ff2ce16
Artyom Sventetsky <artyom.sven@gmail.com>
2022-11-05 21:26:53 +0600

    live-chat-app: create new connectionId record on $connect

--- live-chat-app/source/backend/lambda_live_chat_connect/lambda.py
+++ live-chat-app/source/backend/lambda_live_chat_connect/lambda.py
@@ -1,5 +1,27 @@
+import boto3
+
+from botocore.exceptions import ClientError
+
+client = boto3.client('dynamodb')
+table_name = "live-chat"
+
+
 def handler(event, context):
-    print(f"Websocket event: {event}")
+    connection_id = event['requestContext']['connectionId']
+
+    try:
+        client.put_item(
+            TableName=table_name,
+            Item={
+                'connectionId': {
+                    'S': connection_id
+                }
+            }
+        )
+    except ClientError:
+        print(f"Couldn't put connection_id={connection_id}.")
+        raise
+
     return construct_response()
 
 
