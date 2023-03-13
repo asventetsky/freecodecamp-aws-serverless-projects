fa89d3d5400dec91f4021456a0bb675c547405ae
asventetsky <artyom.sven@gmail.com>
2023-03-13 22:36:49 +0600

    url-shortener-app: implement getting original url

---
+++ url-shortener/source/get_original_url/main.py
@@ -0,0 +1,46 @@
+# pylint: disable=unused-argument
+# pylint: disable=import-error
+
+""" Application logic """
+
+import json
+import logging
+
+from repository import get_record
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    """ Contains main logic for getting original url """
+
+    url_hash = extract_url_hash(event)
+    original_url = get_record(url_hash)
+
+    return construct_response(original_url)
+
+
+def extract_url_hash(event):
+    """ Extracts url hash from the request """
+
+    url_hash = event['pathParameters']['hash']
+
+    logging.info('Url hash: %s', url_hash)
+
+    return url_hash
+
+
+def construct_response(original_url):
+    """ Constructs final response """
+
+    result = {
+        'originalUrl': original_url
+    }
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(result)
+    }
