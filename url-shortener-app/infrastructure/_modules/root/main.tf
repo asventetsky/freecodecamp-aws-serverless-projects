e308e2c05859cf0eba6d2743af387de7d2fb6f3b
asventetsky <artyom.sven@gmail.com>
2023-03-28 23:12:10 +0600

    url-shortener-app: update usage of lambda and dynamo db modules

--- url-shortener-app/infrastructure/_modules/root/main.tf
+++ url-shortener-app/infrastructure/_modules/root/main.tf
@@ -1,3 +1,16 @@
+module "dynamo_db" {
+  source = "../../../../../_modules/dynamo_db"
+
+  region = var.region
+  env = var.env
+  app_name = var.app_name
+
+  resource_tags = var.resource_tags
+}
+
+#===============================================
+# CREATE SHORT URL: lambda function and iam role
+#===============================================
 data "aws_iam_policy_document" "create_short_url" {
   statement {
     actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
@@ -6,29 +19,13 @@ data "aws_iam_policy_document" "create_short_url" {
   }
   statement {
     actions   = ["dynamodb:PutItem"]
-    # TODO replace with link to actual resource
-    resources = ["*"]
+    resources = [module.dynamo_db.dynamo_db_table_arn]
     effect = "Allow"
   }
 }
 
-data "aws_iam_policy_document" "get_original_url" {
-  statement {
-    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
-    resources = ["arn:aws:logs:*:*:*"]
-    effect = "Allow"
-  }
-  statement {
-    actions   = ["dynamodb:GetItem"]
-    # TODO replace with link to actual resource
-    resources = ["*"]
-    effect = "Allow"
-  }
-}
-
-# Create lambda iam role for create-short-url
 module "lambda_create_short_url_iam_role" {
-  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_iam_role?ref=terraform-common-lambda-iam-role-module"
+  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_iam_role?ref=terraform-lambda-zip-archive-module"
 
   region = var.region
   env = var.env
@@ -38,9 +35,35 @@ module "lambda_create_short_url_iam_role" {
   resource_tags = var.resource_tags
 }
 
-# Create lambda iam role for get-original-url
+module "lambda_create_short_url" {
+  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_zip_archive?ref=terraform-lambda-zip-archive-module"
+
+  name = "lambda-create_short_url-${var.region}-${var.env}"
+  path_to_archive = "${path.module}/../../../source/target/${var.lambda_create_short_url_artifact_name}"
+  lambda_role_arn = module.lambda_create_short_url_iam_role.arn
+  handler = "api_composer/main.lambda_handler"
+  environment_variables = {}
+  resource_tags = var.resource_tags
+}
+
+#===============================================
+# GET ORIGINAL URL: lambda function and iam role
+#===============================================
+data "aws_iam_policy_document" "get_original_url" {
+  statement {
+    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = ["dynamodb:GetItem"]
+    resources = [module.dynamo_db.dynamo_db_table_arn]
+    effect = "Allow"
+  }
+}
+
 module "lambda_get_original_url_iam_role" {
-  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_iam_role?ref=terraform-common-lambda-iam-role-module"
+  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_iam_role?ref=terraform-lambda-zip-archive-module"
 
   region = var.region
   env = var.env
@@ -50,12 +73,13 @@ module "lambda_get_original_url_iam_role" {
   resource_tags = var.resource_tags
 }
 
-module "dynamo_db" {
-  source = "../../../../../_modules/dynamo_db"
+module "lambda_get_original_url" {
+  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/lambda_zip_archive?ref=terraform-lambda-zip-archive-module"
 
-  region = var.region
-  env = var.env
-  app_name = var.app_name
-
+  name = "lambda-get_original_url-${var.region}-${var.env}"
+  path_to_archive = "${path.module}/../../../source/target/${var.lambda_get_original_url_artifact_name}"
+  lambda_role_arn = module.lambda_get_original_url_iam_role.arn
+  handler = "get_original_url/main.lambda_handler"
+  environment_variables = {}
   resource_tags = var.resource_tags
 }
