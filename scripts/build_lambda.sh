df1bc7902a8e536fe0ab345d91373e07e9d0c397
asventetsky <artyom.sven@gmail.com>
2023-03-31 16:39:24 +0600

    url-shortener-app: update path to lambda archive

---
+++ scripts/build_lambda.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+main() {
+  LAMBDA_SOURCE_DIR=$1
+  LAMBDA_MODULES="$LAMBDA_SOURCE_DIR $2 __init__.py"
+  echo "LAMBDA_MODULES=$LAMBDA_MODULES"
+  LAMBDA_ARTIFACT_NAME="${LAMBDA_SOURCE_DIR}_artifact.zip"
+  echo "LAMBDA_ARTIFACT_NAME=$LAMBDA_ARTIFACT_NAME"
+
+  echo "Creating a directory for artifact."
+  mkdir -p target
+
+  echo "Installing dependencies."
+  if [ -d "venv" ]
+  then
+    python3 -m venv venv
+  fi
+  source venv/bin/activate
+  pip install -r requirements.txt
+  deactivate
+
+  echo "Creating a package with dependencies."
+  (
+    cd venv/lib/python3.10/site-packages/ || exit
+    zip -q -r ../../../../target/$LAMBDA_ARTIFACT_NAME .
+  )
+
+  echo "Adding source code to the package."
+  (
+    cd "$LAMBDA_SOURCE_DIR" || exit
+    zip -q -g ../target/$LAMBDA_ARTIFACT_NAME $LAMBDA_MODULES
+  )
+}
+
+main "$@"; exit
