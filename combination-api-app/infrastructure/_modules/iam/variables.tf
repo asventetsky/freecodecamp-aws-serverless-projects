1dcce2529ac04471cbfe15c0b7b20ddbe99288a4
asventetsky <artyom.sven@gmail.com>
2023-04-06 22:18:25 +0600

    combination-api-app: remove unused modules

--- combination-api-app/infrastructure/_modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {}
-
-variable "env" {}
-
-variable "app_name" {}
-
-variable "resource_tags" {
-  type = map(string)
-}
--- combination-api-app/infrastructure/_modules/lambda_zip_archive/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.api_combiner.invoke_arn
-}
-
-output "lambda_name" {
-  value = aws_lambda_function.api_combiner.function_name
-}
