a8588cb57580b80e328c1e33a94807c5d4110ac6
asventetsky <artyom.sven@gmail.com>
2023-04-10 11:03:05 +0600

    url-shortener-app: cognito auth

--- url-shortener-app/infrastructure/_modules/root/main.tf
+++ url-shortener-app/infrastructure/_modules/root/main.tf
@@ -95,9 +95,10 @@ module "lambda_get_original_url" {
 # API Gateway #
 #=============#
 module "api_gateway" {
-  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/api_gateway?ref=18afbd80c39696171a67acf2333be7705cba3059"
+  source = "github.com/asventetsky/freecodecamp-aws-serverless-projects-common//terraform/module/aws/api_gateway?ref=terraform-api-gateway-cognito-authorization"
 
   api_gateway_name = "url-shortener-app-${var.region}-${var.env}"
+  cognito_auth = true
   stage = "dev"
 
   integrations = {
